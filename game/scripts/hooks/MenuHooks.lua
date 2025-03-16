ecd14a314482c215de7a9c4dcfb702ac4180e91f
Uladzislau Nikalayevich <thenormalnij@gmail.com>
2025-03-16 21:18:22 +0300

    Fix invalid selection in keepsakes menu

--- game/scripts/hooks/MenuHooks.lua
+++ game/scripts/hooks/MenuHooks.lua
@@ -16,43 +16,65 @@ local HookUtils = ModRequire "../HookUtils.lua"
 local MenuHooks = {}
 
 function MenuHooks.InitHooks()
-    local function hookUiScreen(funName)
-        local originalFun = _G[funName]
-        _G[funName] = function(...)
-            local currentHero = HeroContext.GetCurrentHeroContext()
-            if currentHero == CoopPlayers.GetMainHero() then
-                return originalFun(...)
-            else
-                local prevState = ShallowCopyTable(CoopPlayers.PlayerIdToController)
+    MenuHooks.HookUiControl("ShowWeaponUpgradeScreen")
+    MenuHooks.HookUiControl("ShowAwardMenu")
+    MenuHooks.HookUiControl("HandleLootPickup")
+    MenuHooks.HookUiControl("ShowAdvancedTooltip")
 
-                -- We need first change player 0 controller to requested plater controller
-                -- So the player 1 will control the menu
-                local playerId = TableUtils.find(CoopPlayers.CoopHeroes, currentHero)
-                local controller = CoopPlayers.PlayerIdToController[playerId]
-
-                for playerId, _ in pairs(CoopPlayers.PlayerIdToController) do
-                    if playerId == 1 then
-                        CoopSetPlayerGamepad(playerId, controller)
-                    else
-                        CoopSetPlayerGamepad(playerId, -1)
-                    end
-                end
-
-                HookUtils.onPreFunctionOnce("UnfreezePlayerUnit", function()
-                    for playerId, gamepadId in pairs(prevState) do
-                        CoopSetPlayerGamepad(playerId, gamepadId)
-                    end
-                end)
-
-                originalFun(...)
+    HookUtils.onPreFunction("ShowAwardMenu", function()
+        local currentGift, currentAssist
+        for _, trait in pairs(CurrentRun.Hero.Traits) do
+            if not trait.InheritFrom then
+                goto continue
             end
+
+            if trait.InheritFrom[1] == "AssistTrait" then
+                currentAssist = trait.Name
+            elseif trait.InheritFrom[1] == "GiftTrait" then
+                currentGift = trait.Name
+            end
+
+            ::continue::
+        end
+
+        GameState.LastAwardTrait = currentGift
+        GameState.LastAssistTrait = currentAssist
+    end)
+end
+
+---@private
+---@param funName string
+function MenuHooks.HookUiControl(funName)
+    local originalFun = _G[funName]
+    _G[funName] = function(...)
+        local currentHero = HeroContext.GetCurrentHeroContext()
+        if currentHero == CoopPlayers.GetMainHero() then
+            return originalFun(...)
+        else
+            local prevState = ShallowCopyTable(CoopPlayers.PlayerIdToController)
+
+            -- We need first change player 0 controller to requested plater controller
+            -- So the player 1 will control the menu
+            local playerId = TableUtils.find(CoopPlayers.CoopHeroes, currentHero)
+            local controller = CoopPlayers.PlayerIdToController[playerId]
+
+            for playerId, _ in pairs(CoopPlayers.PlayerIdToController) do
+                if playerId == 1 then
+                    CoopSetPlayerGamepad(playerId, controller)
+                else
+                    CoopSetPlayerGamepad(playerId, -1)
+                end
+            end
+
+            HookUtils.onPreFunctionOnce("UnfreezePlayerUnit", function()
+                for playerId, gamepadId in pairs(prevState) do
+                    CoopSetPlayerGamepad(playerId, gamepadId)
+                end
+            end)
+
+            originalFun(...)
         end
     end
-
-    hookUiScreen("ShowWeaponUpgradeScreen")
-    hookUiScreen("ShowAwardMenu")
-    hookUiScreen("HandleLootPickup")
-    hookUiScreen("ShowAdvancedTooltip")
 end
 
 return MenuHooks
